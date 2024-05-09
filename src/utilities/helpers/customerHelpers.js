9710263b5615641d11441e87c89d17ef9b80258f
Mohankrish007 <mohangarnepudi2001@gmail.com>
2024-05-09 15:01:14 +0530

    Customer helper added

---
+++ src/utilities/helpers/customerHelpers.js
@@ -0,0 +1,12 @@
+export const customersData = [
+    {
+        id: 1,
+        name: "John",
+        email: "john@",
+        phone: "1234567890",
+        address: "123 Main St",
+        city: "New York",
+        state: "NY",
+        zip: "12345"
+    }
+]
